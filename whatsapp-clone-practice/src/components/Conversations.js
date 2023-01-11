7904ba7ef896b323ff0e0d2ebc25da98f7bb974b
Dan Langan <danlangan@Dans-MBP.hsd1.il.comcast.net>
2023-01-11 10:49:28 -0600

    added more things to the conversations component to have all of the coversations pop up that I need when they are started

--- whatsapp-clone-practice/src/components/Conversations.js
+++ whatsapp-clone-practice/src/components/Conversations.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { ListGroupItem, ListGroup } from 'react-bootstrap'
+import { useConversations } from '../context/ConversationsProvider'
 
 export default function Conversations() {
+
+    const { conversations, selectConversationIndex } = useConversations()
     return (
-        <div>
-            Conversations
-        </div>
+        <ListGroup variant="flush">
+        {conversations.map((conversation, index) => (
+            <ListGroupItem 
+            key={index}
+            action
+            onClick={() => selectConversationIndex(index)}
+            >
+                {conversation.recipients.map(r => r.name).join(', ')}
+            </ListGroupItem>
+        ))}
+      </ListGroup>
     )
 };
